d5cd889d80d66fb2cd33759dda0ded9b8c3eac28
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-15 10:51:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,8 +10,22 @@ class Shoe
     @brand
   end
 
-  def color=(color)
-    @color = color
+  def color=(shoe_color)
+    @color = shoe_color
   end
+
+  def size=(shoe_size)
+    @size = shoe_size
+  end
+
+  def material=(shoe_material)
+    @material = shoe_material
+  end
+
+  def condition=(shoe_condition)
+    @condition = shoe_condition
+  end
+
+  
 
 end
